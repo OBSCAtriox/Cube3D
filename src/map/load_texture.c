ff826f43931bb9f3ce356795c76e433436341510
Thiagoht10 <thiagodesouzaht@gmail.com>
2026-03-24 21:33:19 +0000

    adding load_textures

--- src/map/load_texture.c
+++ src/map/load_texture.c
@@ -22,28 +22,112 @@ void open_file(t_data *data, char *path)
     data->fd_load = fd[1];
 }
 
-static int read_texture(char *line, t_data data)
+static char  *cat_path_texture(char *prt)
 {
-    int     i;
+    int i;
+    int j;
+    char *path;
 
     i = 0;
-    if(ft_memmem()) //falta terminar
+    j = 0;
+    while(ft_isspace(prt[i]))
+        i++;
+    path = malloc((ft_strlen(&prt[i]) + 1) * sizeof(char));
+    if(!path)
+        return (NULL);
+    while(prt[i] && prt[i] != '\n')
+    {
+        path[j] = prt[i];
+        j++;
+        i++;
+    }
+    path[j] = '\0';
+    return (path);
+}
+
+static t_texture    *get_slot_texture(t_game *game, char *flag)
+{
+    t_tex *slot;
+
+    slot = &game->tex;
+    if(ft_strcmp(flag, "NO") == 0)
+        return (&slot->north);
+    if(ft_strcmp(flag, "SO") == 0)
+        return (&slot->south);
+    if(ft_strcmp(flag, "WE") == 0)
+        return (&slot->west);
+    if(ft_strcmp(flag, "EA") == 0)
+        return (&slot->east);
+    return (NULL);
+}
+
+static int  upload_texture(t_game *game, char *flag, char *path)
+{
+    t_texture *slot;
+
+    slot = get_slot_texture(game, flag);
+    if(!slot)
+        return (0);
+    slot->img.img_ptr = mlx_xpm_file_to_image(game->mlx, path, &slot->width, &slot->height);
+    if(!slot->img.img_ptr)
+        return (0);
+    slot->img.addr = mlx_get_data_addr(slot->img.img_ptr, &slot->img.bpp, &slot->img.line_len, &slot->img.endian);
+    if(!slot->img.addr)
+        return (0);
+    return (1);
+} 
+
+static char *found_path(char *line, t_data data, char **flag)
+{
+    int     i;
+    char    *prt;
+    char    *path;
+
+    i = 0;
+    while (i < 4)
+    {
+        prt = ft_memmem(line, ft_strlen(line), data.flag_texture[i], 2);
+        if(prt)
+        {
+            if(ft_isspace(prt[2]))
+            {
+                path = cat_path_texture(&prt[2]);
+                if(!path)
+                    return (NULL);
+                *flag = data.flag_texture[i];
+                return (path);
+            }
+        }
+        i++;
+    }
+    return (NULL);
 }
 
 void    load_textures(t_data data, t_game *game)
 {
     char    *line;
-    int     i;
+    char    *path;
+    char    *flag;
     int     n_texture;
 
-    i = 0;
-    n_texture = 4;
-    line = NULL;
-    while(n_texture > 0)
+    n_texture = 0;
+    while (n_texture < 4)
     {
-        line = get_next_line(data.fd_load);
-        if(!line)
-            return;
-        
+        line = ft_gnl(data.fd_load);
+        if (!line)
+            return ;
+        path = found_path(line, data, &flag);
+        if (path)
+        {
+            if (!upload_texture(game, flag, path))
+            {
+                free(path);
+                free(line);
+                return ;
+            }
+            n_texture++;
+            free(path);
+        }
+        free(line);
     }
 }
