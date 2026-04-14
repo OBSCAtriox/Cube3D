b01d97557c4f76d2717b4c5a59c83d70f3c7af42
Tiago_Cruz <cruztiago2005@gmail.com>
2026-04-14 19:00:16 +0100

    init int arrays fc cc

--- src/inits/struct_inits.c
+++ src/inits/struct_inits.c
@@ -6,6 +6,12 @@ void	init_textures(t_ptex *text)
 	text->so = NULL;
 	text->we = NULL;
 	text->ea = NULL;
+	text->fc[0] = 0;
+	text->fc[1] = 0;
+	text->fc[2] = 0;
+	text->cc[0] = 0;
+	text->cc[1] = 0;
+	text->cc[2] = 0;
 }
 
 void	init_data(t_data *data)
@@ -16,8 +22,6 @@ void	init_data(t_data *data)
 	data->map_max_col = 0;
 	data->map_max_rows = 0;
 	data->num_lines = 0;
-	data->mlx = NULL;
-	data->win = NULL;
 	data->fd_check = 0;
 	data->fd_load = 0;
 	init_textures(&data->textures);
